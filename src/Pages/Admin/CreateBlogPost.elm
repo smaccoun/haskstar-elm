f85e39dc51f9dbbfa3ce36a0db0d93ed48036fb0
Steven MacCoun <theraccoun@gmail.com>
2018-04-12 12:31:39 -0600

    preview blog post content

--- src/Pages/Admin/CreateBlogPost.elm
+++ src/Pages/Admin/CreateBlogPost.elm
@@ -1,13 +1,67 @@
 module Pages.Admin.CreateBlogPost exposing (..)
 
-import Html exposing (Html, div, text)
-import Form
+import Bulma.Columns exposing (column, columnModifiers, columns, columnsModifiers)
+import Bulma.Form as BForm exposing (controlInput, controlInputModifiers, controlText, field)
+import Html exposing (Html, div, input, label, text)
+import Html.Events exposing (onInput)
 
-type Msg =
-  FormMsg Form.Msg
 
-view : Html Msg
-view =
+init : BlogPost
+init =
+    { title = ""
+    , content = ""
+    }
+
+
+type alias BlogPost =
+    { title : String
+    , content : String
+    }
+
+
+type Msg
+    = SetTitle String
+    | SetContent String
+
+
+update : Msg -> BlogPost -> BlogPost
+update msg post =
+    case msg of
+        SetTitle title ->
+            { post | title = title }
+
+        SetContent content ->
+            { post | content = content }
+
+
+view : BlogPost -> Html Msg
+view post =
+    columns columnsModifiers
+        []
+        [ column columnModifiers [] [ viewEditSection ]
+        , column columnModifiers [] [ viewPreviewSection post ]
+        ]
+
+
+viewEditSection : Html Msg
+viewEditSection =
     div []
-    [text "Make a blog!"
-    ]
+        [ text "Make a blog!"
+        , viewInputField "Title" SetTitle
+        , viewInputField "Content" SetContent
+        ]
+
+
+viewPreviewSection : BlogPost -> Html Msg
+viewPreviewSection post =
+    div []
+        [ text post.content
+        ]
+
+
+viewInputField : String -> (String -> Msg) -> Html Msg
+viewInputField label msgC =
+    field []
+        [ BForm.controlLabel [] [ text label ]
+        , controlInput controlInputModifiers [] [ onInput msgC ] []
+        ]
--- src/Pages/Admin/Index.elm
+++ src/Pages/Admin/Index.elm
@@ -1,23 +1,25 @@
 module Pages.Admin.Index exposing (..)
 
-import Server.Config
-import Pages.Admin.Home as Home
-import Pages.Admin.CreateBlogPost as CreateBlogPost
 import Html exposing (Html)
+import Pages.Admin.CreateBlogPost as CreateBlogPost
+import Pages.Admin.Home as Home
+import Server.Config
 import UrlParser as Url exposing ((</>), (<?>), s, top)
 
 
 type AdminPage
     = AdminHome
-    | CreateBlogPost
+    | CreateBlogPost CreateBlogPost.BlogPost
+
 
 type AdminRoute
     = AdminHomeRoute
     | CreateBlogPostRoute
 
-type AdminPageMsg =
-  HomeMsg Home.Msg
-  | CreateBlogPostMsg CreateBlogPost.Msg
+
+type AdminPageMsg
+    = HomeMsg Home.Msg
+    | CreateBlogPostMsg CreateBlogPost.Msg
 
 
 initializePageFromRoute : Server.Config.Context -> AdminRoute -> AdminPage
@@ -25,26 +27,42 @@ initializePageFromRoute serverContext route =
     case route of
         AdminHomeRoute ->
             AdminHome
-        CreateBlogPostRoute ->
-            CreateBlogPost
 
-update : Server.Config.Context -> AdminPage -> AdminPageMsg -> (AdminPage, Cmd AdminPageMsg)
+        CreateBlogPostRoute ->
+            CreateBlogPost CreateBlogPost.init
+
+
+update : Server.Config.Context -> AdminPage -> AdminPageMsg -> ( AdminPage, Cmd AdminPageMsg )
 update serverContext adminPage msg =
-  case msg of
-    HomeMsg homeMsg ->
-      (adminPage, Cmd.none)
-    CreateBlogPostMsg bmsg ->
-      (adminPage, Cmd.none)
+    case msg of
+        HomeMsg homeMsg ->
+            ( adminPage, Cmd.none )
+
+        CreateBlogPostMsg bmsg ->
+            case adminPage of
+                CreateBlogPost blogPost ->
+                    let
+                        updatedPage =
+                            CreateBlogPost.update bmsg blogPost
+                    in
+                    ( CreateBlogPost updatedPage, Cmd.none )
+
+                _ ->
+                    ( adminPage, Cmd.none )
+
 
 viewAdminPage : Server.Config.Context -> AdminPage -> Html AdminPageMsg
 viewAdminPage context adminPage =
-  case adminPage of
-    AdminHome -> Html.map HomeMsg Home.view
-    CreateBlogPost -> Html.map CreateBlogPostMsg CreateBlogPost.view
+    case adminPage of
+        AdminHome ->
+            Html.map HomeMsg Home.view
+
+        CreateBlogPost blogPost ->
+            Html.map CreateBlogPostMsg <| CreateBlogPost.view blogPost
 
 
 routes : List (Url.Parser (AdminRoute -> a) a)
 routes =
-        [ Url.map AdminHomeRoute (s "admin" </> s "home")
-        , Url.map CreateBlogPostRoute (s "admin" </> s "blogPost" </> s "new")
-        ]
+    [ Url.map AdminHomeRoute (s "admin" </> s "home")
+    , Url.map CreateBlogPostRoute (s "admin" </> s "blogPost" </> s "new")
+    ]
